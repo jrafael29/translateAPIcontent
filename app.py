8510cb59b4779badadc1b8a6018149fa9fb205b1
José Rafael <rafaelsilva9240@hotmail.com>
2022-02-27 13:46:16 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from cgitb import text
+from flask import Flask, render_template
+import requests
+from googletrans import Translator
+
+def traduzir(advice):
+    translator = Translator()
+    adviceTraduzido = translator.translate(advice, src='en',dest='ar').text
+    return adviceTraduzido
+    
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    req = requests.get('https://api.adviceslip.com/advice').json()
+    id = req['slip']['id']
+    advice = traduzir(req['slip']['advice'])
+    return render_template('index.html', id=id, advice=advice)
+
+if __name__ == "__main__":
+    app.run(debug=True)
